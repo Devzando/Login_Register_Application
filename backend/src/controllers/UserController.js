7a623529d816be27acdfb4fdc0a909404c9717ac
Gabriel Lira <devzando8182@gmail.com>
2021-01-11 22:33:20 -0300

    Adicionando hash para o password e separando em um a pasta/arquivo diferente a função que gera esse hash e compara também

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,12 +1,14 @@
 const knex = require('../database/connection');
+const { encrypt, decrypt } = require('../helpers/cryptography');
 
 module.exports = {
     async create(req, res) {
         const {email, password} = req.body;
+        const hash = encrypt(String(password));
 
         try {
-            const result = await knex('users').insert({email, password});
-            return res.json(result);
+            await knex('users').insert({email, password: hash});
+            return res.json({email, hash});
         } catch (error) {
             console.log(error);
         }
---
+++ backend/src/helpers/cryptography.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcrypt');
+
+function encrypt(password){
+    return bcrypt.hashSync(password, 10);
+}
+
+function decrypt(newPassword, hash){
+    return bcrypt.compareSync(newPassword, hash)
+}
+
+module.exports = { encrypt, decrypt }
