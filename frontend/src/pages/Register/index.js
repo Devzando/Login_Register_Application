1f6f8211426d4696a281e74fe4013ac6b6f52815
Gabriel Lira <devzando8182@gmail.com>
2021-01-14 01:11:29 -0300

    Componente register estruturado e estilizado

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { useFonts } from 'expo-font'
 
+import SignUpSvg from '../../assets/sign up.svg';
 import styles from './styles';
 
-export default function Register(){
-    return <Text>Hello Register</Text>
+export default function Register() {
+
+    const [loaded] = useFonts({
+        RobotoCondensed: require('../../assets/fonts/RobotoCondensed-Light.ttf')
+    })
+    if (!loaded) return null;
+
+    return (
+        <View style={styles.fist_container}>
+            <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+                <ScrollView
+                    showsVerticalScrollIndicator={true}
+                    showsHorizontalScrollIndicator={true}
+                    keyboardShouldPersistTaps={'always'}
+
+                >
+                    <View style={styles.container}>
+                        <SignUpSvg width={315} height={230} />
+                        <Text style={[styles.text_sign, { fontFamily: 'RobotoCondensed' }]}>Sign Up</Text>
+                        <View style={styles.container_input}>
+                            <Text style={[styles.text_input,
+                            { fontFamily: 'RobotoCondensed', marginTop: 31, marginRight: 265 }
+                            ]}>E-mail</Text>
+                            <View style={styles.input}>
+                                <TextInput keyboardType={'email-address'} />
+                            </View>
+                            <Text style={[styles.text_input, { fontFamily: 'RobotoCondensed', marginTop: 42 }]}>Password</Text>
+                            <View style={styles.input}>
+                                <TextInput secureTextEntry />
+                            </View>
+                            <TouchableOpacity
+                                style={styles.button}
+                            >
+                                <Text style={[styles.text_button, { fontFamily: 'RobotoCondensed' }]}>Register</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </View>
+    )
 }
--- frontend/src/pages/Register/styles.js
+++ frontend/src/pages/Register/styles.js
@@ -1,7 +1,64 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import constants from 'expo-constants';
 
 export default StyleSheet.create({
+    fist_container:{
+        flex: 1,
+        backgroundColor: '#C6C2DF'
+    },
     container:{
-        
+        marginTop: constants.statusBarHeight + 43,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text_sign:{
+        fontSize: 48,
+        lineHeight: 55,
+        letterSpacing: 0.09,
+        textShadowColor: 'rgba(0, 0, 0, 0.28)',
+        textShadowOffset: {width: 4, height: 4},
+        textShadowRadius: 4,
+        color: '#0E00F3',
+        opacity: 0.74,
+        marginTop: 22 
+    },
+    container_input:{
+        backgroundColor: '#6C63FF',
+        width: '100%',
+        height: Dimensions.get('window').height -330,
+        borderTopLeftRadius: 73,
+        alignItems: 'center',
+        marginTop: 25
+    },
+    text_input:{
+        fontSize: 18,
+        lineHeight: 25,
+        letterSpacing: 0.09,
+        marginRight: 240
+    },
+    input:{
+        width: Dimensions.get('window').width -48,
+        marginLeft: 6,
+        marginTop: 7,
+        borderBottomWidth: 1,
+        borderBottomColor: '#000'
+    },
+    button:{
+        backgroundColor: '#C6C2DF',
+        borderRadius: 8,
+        borderColor: '#000',
+        borderWidth: 2,
+        marginTop: 55,
+        marginBottom: 55,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: Dimensions.get('window').width -120,
+        height: 51
+    },
+    text_button:{
+        fontSize: 34,
+        lineHeight: 39,
+        letterSpacing: 0.09
     }
+    
 })
